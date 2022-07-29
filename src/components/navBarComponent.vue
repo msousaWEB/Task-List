235e7080d6cd96529711f054929837f22b5a749d
msousa <msousa.dev@outlook.com>
2022-07-28 23:29:34 -0300

    creating menu and task list view

---
+++ src/components/navBarComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <b-navbar toggleable="lg" type="dark" variant="info">
+            <b-navbar-brand href="#">To-Do List</b-navbar-brand>
+
+            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+            <b-collapse id="nav-collapse" is-nav>
+                <b-navbar-nav>
+                    <b-nav-item to="/list">Tarefas</b-nav-item>
+                </b-navbar-nav>
+            </b-collapse>
+        </b-navbar>
+    </div>
+</template>
+<script>
+export default {
+    name: 'navBarComponent'
+}
+</script>
---
+++ src/views/ListView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div class="d-flex justify-content-center">
+      <div class="col-6 mt-4">
+        <b-card title="Card title">
+          <b-card-text>
+            Some quick example text to build on the <em>card title</em> and make up the bulk of the card's
+            content.
+          </b-card-text>
+
+          <b-button variant="info" pill style="margin-right:4px">Editar</b-button>
+          <b-button variant="danger" pill >Excluir</b-button>
+
+        </b-card>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ListView'
+}
+</script>
